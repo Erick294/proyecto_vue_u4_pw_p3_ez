9614a414d654b2db25d0e929dcd5b9a50a00ee64
Erick294 <beowulfblackman@hotmail.com>
2023-02-22 19:48:37 -0500

    Taller 33

---
+++ src/js/api_facturacion/ProcesarCliente.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+//Metodos fachada
+
+//Metodos consumo CRUD
+const obtenerClientePorCedula = (cedula) => {
+
+}
+
+const insertar = (body) => {
+
+}
+
+const actualizar = (cedula, body) => {
+
+}
+ 
