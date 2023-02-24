bbdfd7147977332677bb5ae684551fc9bf2f8fe7
Erick294 <beowulfblackman@hotmail.com>
2023-02-23 19:47:35 -0500

    Taller 34

--- src/js/api_facturacion/ProcesarCliente.js
+++ src/js/api_facturacion/ProcesarCliente.js
@@ -1,17 +1,58 @@
 import axios from "axios";
 
 //Metodos fachada
+//Export en cada fachada permite exponerlas
+export const obtenerPorCedulaFachada = async (cedula) => {
+    return await obtenerPorCedula(cedula);
+}
+
+export const insertarFachada = async (body) => {
+    await insertar(body);
+}
+
+export const actualizarFachada = async (id, body) => {
+    return await actualizar(id, body);
+}
+
+export const buscarTodosFachada = async () => {
+    return await buscarTodos()
+}
+
+export const borrarIdFachada = async (id) => {
+    await borrarId(id);
+}
+
+export const borrarPorCedulaFachada = async (cedula) => {
+    await borrarPorCedula(cedula);
+}
 
 //Metodos consumo CRUD
-const obtenerClientePorCedula = (cedula) => {
-
+const obtenerPorCedula = (cedula) => {
+    const data = axios.get(`http://localhost:8081/API/Facturacion/V1/clientes/${cedula}`).then(r=>r.data)
+    console.log(data)
+    return data
 }
 
 const insertar = (body) => {
+    axios.post(`http://localhost:8081/API/Facturacion/V1/clientes`, body).then(r=>r.data)
+}
+
+const actualizar = (id, body) => {
+    axios.put(`http://localhost:8081/API/Facturacion/V1/clientes/${id}`, body).then(r=>r.data)
+}
+
+const buscarTodos = () => {
+    const data = axios.get(`http://localhost:8081/API/Facturacion/V1/clientes`).then(r=>r.data)
+    return data
+}
+
+const borrarId = (id) => {
+    axios.delete(`http://localhost:8081/API/Facturacion/V1/clientes/${id}`).then(r=>r.data)
+}
+
+const borrarPorCedula = (cedula) => {
 
 }
 
-const actualizar = (cedula, body) => {
-
-}
+//export default obtenerPorCedulaFachada
  
---
+++ src/pages/BuscarPorCedula.vue
@@ -0,0 +1,56 @@
+<template>
+  <h1>Buscar por cedula</h1>
+  <input type="text" v-model="cedula">
+  <button v-on:click="consultarCliente()">Buscar</button>
+  <button v-on:click="insertarCliente()">Insertar</button>
+  <button v-on:click="actualizarCliente()">Actualizar</button>
+  <button v-on:click="eliminarCliente()">Eliminar</button>
+
+</template>
+
+<script>
+import {obtenerPorCedulaFachada, insertarFachada, actualizarFachada, borrarIdFachada} from "../js/api_facturacion/ProcesarCliente.js"
+
+export default {
+    data(){
+        return{
+            cedula: null
+        }
+    },
+    methods:{
+        async consultarCliente(){
+            const data = await obtenerPorCedulaFachada(this.cedula)
+            console.log(data)
+        },
+        async insertarCliente(){
+            const miCliente = {
+                nombre: "Fabricio",
+                apellido: "Chacon",
+                fechaNacimiento: "1997-09-20T00:00:00",
+                cedula: "1234567",
+                genero: "Masculino",
+                email:"jhksddfkjs@gmail.com"
+            }
+            await insertarFachada(miCliente)
+        }, 
+        async actualizarCliente(){
+            const miCliente = {
+                nombre: "Manuel",
+                apellido: "Galarza",
+                fechaNacimiento: "1997-09-20T00:00:00",
+                cedula: "12345678",
+                genero: "Masculino",
+                email:"jhksddfkjs@gmail.com"
+            }
+            await actualizarFachada(2, miCliente)
+        },
+        async eliminarCliente(){
+            await borrarIdFachada(2)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
