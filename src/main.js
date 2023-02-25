b44ded9c714afb45d96bfe0d642200a5a7dff502
Erick294 <85328371+Erick294@users.noreply.github.com>
2023-02-25 17:39:49 -0500

    Taller 38

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import router from '../src/router/router.js'
 
 createApp(App).use(router).mount('#app')
